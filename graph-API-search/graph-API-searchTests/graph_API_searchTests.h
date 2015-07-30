cdc416f73cd8cde9f9df4f8149a73c3cf830a6be
RAGOpoR Sharp <ragopor@me.com>
2015-07-30 13:39:09 +0700

    Migrate to xctest and add circle.yml

--- graph-API-search/graph-API-searchTests/graph_API_searchTests.h
+++ graph-API-search/graph-API-searchTests/graph_API_searchTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Siam Wannakosit. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface graph_API_searchTests : SenTestCase
+@interface graph_API_searchTests : XCTestCase
 
 @end
--- graph-API-search/graph-API-searchTests/graph_API_searchTests.m
+++ graph-API-search/graph-API-searchTests/graph_API_searchTests.m
@@ -26,7 +26,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in graph-API-searchTests");
+    XCTFail(@"Unit tests are not implemented yet in graph-API-searchTests");
 }
 
 @end
